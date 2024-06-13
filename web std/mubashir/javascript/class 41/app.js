1d9a2da531f3e479d362ebdc77e44116d9a0f256
Rafiullah <140521887+Rafiullah08@users.noreply.github.com>
2024-06-13 07:27:31 -0700

    stopWatch complete with full functions

--- web std/mubashir/javascript/class 41/app.js
+++ web std/mubashir/javascript/class 41/app.js
@@ -1,577 +1,82 @@
+var hrs = 0;
+var mnt = 0;
+var sec = 0;
+var mili = 0;
 
+var milsec = document.getElementById("Milli");
+var seconds = document.getElementById("seconds");
+var minuts = document.getElementById("Minuts");
+var hours = document.getElementById("Hours");
 
-var hrs = 0
-var mnt = 0
-var sec = 0
-var mili = 0
+var timer = false;
 
-var timer = false
-
-
-var milsec = document.getElementById("Milli")
-var seconds = document.getElementById("seconds")
-var minuts = document.getElementById("Minuts")
-var hours = document.getElementById("Hours")
-
-
-function start(){
-    timer = true
-myFun()
-
+function start() {
+  timer = true;
+  myFun();
 }
 
-function myFun(){
+function myFun() {
+  if (timer == true) {
+    mili = mili + 1;
+    milsec.innerHTML = mili;
 
-    if(timer == true){
-        mili = mili+ 1
-milsec.innerHTML = mili
+    if (mili == 100) {
+      sec = sec + 1;
+      seconds.innerHTML = sec;
+      mili = 0;
 
-if(mili == 100){
-    sec = sec+ 1
-    seconds.innerHTML = sec
-    mili = 0
+      if (sec == 60) {
+        mnt = mnt + 1;
+        minuts.innerHTML = mnt;
+        sec = 0;
 
-if(sec == 10){
-    mnt = mnt+ 1
-    minuts.innerHTML = mnt
-    sec = 0
-
-    if(mnt == 1){
-        hrs = hrs+ 1
-        hours.innerHTML = hrs
-        mnt = 0
-
-
-
-  
-    }
-}
-
-}
-
-
-
-
-
-interval = setTimeout(myFun, 10)
-
+        if (mnt == 5) {
+          hrs = hrs + 1;
+          hours.innerHTML = hrs;
+          mnt = 0;
+        }
+      }
     }
 
-    var getmili = document.getElementById("Milli")
-    var getsec = document.getElementById("seconds")
-    // var getmnt = document.getElementById("Minuts")
-    // var gethrs = document.getElementById("Hours")
-    
-    // if(getsec< 10){
-    //     getsec.innerHTML = "0" + getsec
-    // }
-    
-console.log(getsec.innerHTML)
+    var getsec = sec;
+    var getmnt = mnt;
+    var gethr = hrs;
+    var getmilli = mili;
 
+    if (getsec < 10) {
+      getsec = "0" + sec;
+    }
+    if (getmnt < 10) {
+      getmnt = "0" + mnt;
+    }
+    if (gethr < 10) {
+      gethr = "0" + hrs;
+    }
+    if (mili < 10) {
+      getmilli = "0" + mili;
+    }
+
+    seconds.innerHTML = getsec;
+    minuts.innerHTML = getmnt;
+    hours.innerHTML = gethr;
+    milsec.innerHTML = getmilli;
+
+    setTimeout(myFun, 10);
+  }
+}
+
+// create reset funtion
+function reset() {
+  location.reload();
 }
 
 
 
 
-function reset(){
-location.reload()
+// create stop function
+function stop() {
+  // clearInterval(interval)
+
+  timer = false;
+  myFun();
 }
-
-
-
-
-function stop(){
-clearInterval(interval)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
