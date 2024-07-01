10f8af13abd081e1b89da9d73ae280ae05d66e63
Rafiullah <140521887+Rafiullah08@users.noreply.github.com>
2024-07-01 18:38:29 +0500

    object start

---
+++ web std/mubashir/javascript/class 42/app.js
@@ -0,0 +1,16 @@
+//  objects 
+//   var arr = [] 
+var obj = {
+name : "rafiullah",
+age : 23,
+
+}
+console.log([obj])
+
+//  obj ko call krne k 2 tariqe hai
+// 1 ; dot notation
+// 2; arr notation
+ 
+
+// console.log(obj.age)
+
