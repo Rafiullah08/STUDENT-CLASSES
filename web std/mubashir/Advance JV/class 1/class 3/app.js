79a70bf57ea287f55de659fb0186a1eb0f476361
Rafiullah <140521887+Rafiullah08@users.noreply.github.com>
2024-08-25 14:26:49 -0700

    arrow function and array methods

---
+++ web std/mubashir/Advance JV/class 1/class 3/app.js
@@ -0,0 +1,45 @@
+
+// es 5
+// function add(){
+//     console.log("normal function")
+// }
+
+
+// add()
+
+
+// Es 6
+
+
+
+// const arrFun =()=>{
+//  console.log( "arrow function")
+// }
+
+// const newFun =()=> console.log(23+334)
+// arrFun()
+
+// console.log(arrFun)
+
+
+// map , filter, reduce
+
+//  let arr = [34,43,45,5,6,67,8]
+
+//     arr.map((value,num )=>{
+
+// console.log(value)
+//  })
+//  arr.filter((data)=>{
+
+    // console.log(data)
+//  data * 2
+
+
+// })
+
+// console.log(arr)
+
+
+
+ 
