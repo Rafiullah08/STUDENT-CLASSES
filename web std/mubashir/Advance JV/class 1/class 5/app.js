7b47164e65aa04d3ab391f96847be242c20699d0
Rafiullah <140521887+Rafiullah08@users.noreply.github.com>
2024-08-31 14:09:29 -0700

    ternary operator and reduce

---
+++ web std/mubashir/Advance JV/class 1/class 5/app.js
@@ -0,0 +1,53 @@
+
+
+// reduce method
+let arr = [23,34,45,55,66]
+
+let red = arr.reduce((prev,curr)=>{
+return prev + curr
+})
+
+console.log(red)
+
+
+
+// ternory operator
+
+let stdAge = prompt("enetr your age")
+
+// if(stdAge  == 20){
+//     console.log("welcome")
+// }else{
+//     console.log("not allowed")
+
+// }
+
+let ter = stdAge >= 20 ? "allow" :"not allow"
+console.log(ter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
