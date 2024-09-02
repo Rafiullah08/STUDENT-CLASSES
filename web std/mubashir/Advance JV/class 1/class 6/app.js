71deb827ef21aa20859221c9dfb91e121523faef
Rafiullah <140521887+Rafiullah08@users.noreply.github.com>
2024-09-02 12:20:41 -0700

    promises

---
+++ web std/mubashir/Advance JV/class 1/class 6/app.js
@@ -0,0 +1,29 @@
+
+
+// Promises
+
+console.log("hello world 1")
+
+
+
+
+
+console.log("hello world 2")
+
+// synchronous  
+// asynchronous
+ 
+// 1-  promises
+
+
+let promise = new Promise(function(resolve, reject){
+
+
+console.log("hello promise")
+
+})
+.then((resolve)=>console.log(resolve))
+.catch((reject)=>console.log(reject))
+
+
+// console.log(promise)
