4b41077c1f1fb149b378521c29e2bcd0e2c31fa5
Rafiullah <140521887+Rafiullah08@users.noreply.github.com>
2025-03-09 12:52:48 -0700

    react start class 1

---
+++ web std/mubashir/class 1/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import imgg from './assets/bybit.jpg'
+import './App.css'
+import Button from './components/buttons'
+
+
+function App() {
+
+
+  return (
+// fragment
+<>
+     <h1>hello</h1>
+     <h1>hello</h1>
+     <h1>hello</h1>
+    <Button/> 
+<img src={imgg} alt="" />
+</>
+
+  )
+}
+
+export default App
---
+++ web std/mubashir/class 1/src/components/buttons.jsx
@@ -0,0 +1,11 @@
+
+
+
+function Button(){
+    return(
+    <button>Submitt</button>
+    )
+}
+
+
+export default Button
